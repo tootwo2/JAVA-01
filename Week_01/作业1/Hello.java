245f686dddcd03ceff6f6714a02fc44f82219249
tootwo2 <roc_tootwo@163.com>
2021-01-12 22:21:29 +0800

    第一周作业1

---
+++ Week_01/作业1/Hello.java
@@ -0,0 +1,20 @@
+public class Hello {
+  public static void main(String[] args) {
+
+    //new对象
+    Hello hello = new Hello();
+
+    //四则运行
+    int num1 = 10;
+    int num2 = num1 + 2;
+
+    //StringBuilder
+    String s1 = "aa";
+    String s2 = s1 + "bb";
+
+    if(s2 != s1){
+      System.out.print("N");
+    }
+
+  }
+}
